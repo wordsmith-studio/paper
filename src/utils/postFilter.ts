dcca60430353fd4d4b1a6b107457b27ad3cba9fa
achuinard <tony@twansoftware.com>
2024-05-05 16:09:58 -0400

    Filt

--- src/utils/postFilter.ts
+++ src/utils/postFilter.ts
@@ -2,7 +2,7 @@ import { SITE } from "@config";
 import type { CollectionEntry } from "astro:content";
 
 const postFilter = ({ data }: CollectionEntry<"blog">) => {
-  return false;
+  return true;
 };
 
 export default postFilter;
