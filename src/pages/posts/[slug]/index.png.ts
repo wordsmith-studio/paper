6b42a7106877232b81d2f2bf0fb5334bcc4bd432
achuinard <tony@twansoftware.com>
2024-05-11 01:45:39 -0400

    Fix

--- src/pages/posts/[slug]/index.png.ts
+++ src/pages/posts/[slug]/index.png.ts
@@ -9,7 +9,7 @@ export async function getStaticPaths() {
   );
 
   return posts.map(post => ({
-    params: { slug: slugifyStr(post.data.title) },
+    params: { slug: post.slug },
     props: post,
   }));
 }
